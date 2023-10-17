a77cf37180986fac645b8bead6e45770d84c55bd
Owolabi Samuel <samueladelekeowolabi@gmail.com>
2023-10-16 18:23:47 -0700

    reset 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the function main
+ * @n: function parameter
+ * Return: 1 for success or 0 for failure
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is a readme file for Pointers, Arrays and Strings

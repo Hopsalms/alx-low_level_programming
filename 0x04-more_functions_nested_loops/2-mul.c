0a984b9530e7312fe00a4b0bc9e9aea4417aa4e6
Owolabi Samuel <samueladelekeowolabi@gmail.com>
2024-06-27 19:07:23 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that mltiplies two integers
+ * @a: paramerer 1
+ * @b: parameter 2
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

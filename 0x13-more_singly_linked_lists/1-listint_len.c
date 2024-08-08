6bd657efdd0438c8454614d63b1afad9ecb140f3
Owolabi Samuel <samueladelekeowolabi@gmail.com>
2024-08-07 19:58:46 -0700

    length of list integer

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		count += 1;
+		current = current->next;
+	}
+	return (count);
+}
